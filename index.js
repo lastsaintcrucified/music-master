f6cf52a5a20a5527536dda36e5efe7904df6ca88
unknown <lastsaintcrucified@gmail.com>
2019-11-14 23:08:36 +0600

    changes in router

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.css';
-ReactDOM.render(<App/>,document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import BrowserRouter from "react-router-dom/BrowserRouter";
+import App from "./components/App";
+import "./index.css";
+ReactDOM.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
